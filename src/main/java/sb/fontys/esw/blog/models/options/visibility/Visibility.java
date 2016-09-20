279f1d20412de275152cb1ceaf57cb5379a606ae
Robert <rversteegt@live.nl>
2016-09-20 15:05:20 +0200

    even more stuff

--- src/main/java/sb/fontys/esw/blog/models/options/visibility/Visibility.java
+++ src/main/java/sb/fontys/esw/blog/models/options/visibility/Visibility.java
@@ -6,16 +6,4 @@ package sb.fontys.esw.blog.models.options.visibility;
  */
 public enum Visibility {
     SIMPLE, ADVANCED;
-    
-    public <T> T match(T simple, T advanced) {
-        switch(this) {
-            case SIMPLE: 
-                return simple;
-            case ADVANCED:
-                return advanced;
-            default:
-                throw new RuntimeException(
-                        "So much for structural pattern matching in Java.");
-        }
-    }
 }
